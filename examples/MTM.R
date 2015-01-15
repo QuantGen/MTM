e71ceca967d8e3bfc9a619848f39fd457c8ae39f
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2015-01-15 10:43:23 -0600

    Do not run examples as BGLR dependency is not satisfied.

--- examples/MTM.R
+++ examples/MTM.R
@@ -1,7 +1,8 @@
+\dontrun{
 library(BGLR)
 
 data(wheat)
-G <- tcrossprod(scale(wheat.X, center = TRUE, scale = TRUE))/ncol(wheat.X)
+G <- tcrossprod(scale(wheat.X, center = TRUE, scale = TRUE)) / ncol(wheat.X)
 
 nTraits <- ncol(wheat.Y)
 df0 <- 3
@@ -11,8 +12,10 @@ R2 <- 0.5
 # One can provide K or EVD via V and d, I believe. Use much longer chains, this
 # is just an example!
 fm <- MTM(Y = wheat.Y,
-          K = list(list(K = G, COV = list(type = "UN", df0 = (df0 + nTraits), S0 = diag(R2 * (df0 + 2 * nTraits + 1), nTraits)))),
-          resCov = list(type = "DIAG", df0 = rep(df0, nTraits), S0 = diag(R2 * df0/(df0 + 2), nTraits)),
+          K = list(list(K = G, COV = list(type = "UN", df0 = (df0 + nTraits),
+                S0 = diag(R2 * (df0 + 2 * nTraits + 1), nTraits)))),
+          resCov = list(type = "DIAG", df0 = rep(df0, nTraits),
+                S0 = diag(R2 * df0/(df0 + 2), nTraits)),
           nIter = 500,
           burnIn = 100)
 
@@ -20,3 +23,4 @@ fm <- MTM(Y = wheat.Y,
 fm$K[[1]]$G  # Estimated genomic covariance matrix
 fm$K[[1]]$U  # Predicted genomic values
 fm$resCov$R  # Estimated residual covariance matrix
+}
